f5b5e6be3bb5fe05d81bf407faa32b5f76259d68
First Last <170429152+ReCodes26@users.noreply.github.com>
2026-09-24 22:03:56 -0400

    Created getBookmark Function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react";
-import { getAllBookmarks } from "./Bookmark/BookmarkFunctions";
+import { getBookmark } from "./Bookmark/bookmarkFunctions";
 
 function App() {
   useEffect(() => {
-    console.log(getAllBookmarks());
+    console.log(getBookmark("213321"));
   }, []);
   return (
     <h1 className="text-3xl font-bold underline text-blue-400">Hello world!</h1>
--- src/Bookmark/bookmarkFunctions.ts
+++ src/Bookmark/bookmarkFunctions.ts
@@ -1,5 +1,16 @@
 import type { Bookmark } from "../Data/bookmarkInterface";
 export function createBookmark() {}
+export function getBookmark(ID: string): Bookmark | null {
+  const bookmarks = getAllBookmarks();
+  if (bookmarks) {
+    const bookmark = bookmarks.find((bookmark) => bookmark.ID === ID);
+    if (bookmark) {
+      return bookmark;
+    }
+  }
+
+  return null;
+}
 export function getAllBookmarks(): Bookmark[] | null {
   const bookmarksString = localStorage.getItem("bookmarks");
   if (bookmarksString) {
